e1ba75393be2db4b46071ece7318cc793a3ee0bd
hanandeek21 <hanan.odehdeek1@gmail.com>
2026-08-14 15:31:48 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -141,47 +141,12 @@
           <p class="text-body-2 text-grey">Try adjusting your search query or selected category filter.</p>
         </v-card>
       </v-container>
-
-      <!-- Product Dialog & Reviews -->
-      <v-dialog v-model="dialog" max-width="700">
-        <v-card rounded="xl" v-if="selectedProduct" class="overflow-hidden">
-          <v-row no-gutters>
-            <v-col cols="12" md="6">
-              <v-img :src="selectedProduct.image" height="100%" min-height="280" cover></v-img>
-            </v-col>
-            <v-col cols="12" md="6" class="pa-6 d-flex flex-column">
-              <div class="d-flex justify-space-between align-center mb-2">
-                <v-chip size="small" class="custom-chip" variant="tonal">{{ selectedProduct.category }}</v-chip>
-                <v-btn icon="mdi-close" variant="text" size="small" @click="dialog = false"></v-btn>
-              </div>
-
-              <h2 class="text-h6 font-weight-bold mb-2 text-dark">{{ selectedProduct.name }}</h2>
-
-              <div class="d-flex align-center mb-3">
-                <v-rating :model-value="selectedProduct.rating" color="amber-darken-2" density="compact" size="small" half-increments readonly></v-rating>
-                <span class="text-caption ml-2 text-grey">({{ selectedProduct.rating }})</span>
-              </div>
-
-              <p class="text-body-2 text-grey-darken-1 mb-4 flex-grow-1">
-                {{ selectedProduct.description }}
-              </p>
-
-              <div class="d-flex align-center justify-space-between mb-4">
-                <span class="text-h5 font-weight-black text-dark">${{ selectedProduct.price }}</span>
-                <span class="text-caption text-success font-weight-bold"><v-icon icon="mdi-truck-check-outline" class="mr-1"/> Free Shipping</span>
-              </div>
-
-              <v-btn block class="btn-dark-custom size-large mb-4 font-weight-bold" rounded="lg" @click="addToCart(selectedProduct)">
-                Add To Cart
-              </v-btn>
-            </v-col>
-          </v-row>
-
-          <v-divider></v-divider>
-
-          
-        </v-card>
-      </v-dialog>
+<!-- Product Details -->
+<ProductDetailsDialog
+  v-model="dialog"
+  :product="selectedProduct"
+  @add-to-cart="addToCart"
+/>
 
    
 
